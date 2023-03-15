b070a4e905c5dd13a4e5e1d351d1c842f4312616
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2023-03-15 16:24:25 -0400

    set up eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Login from "./Login";
-import { Navigate, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './Login';
 
 function App() {
   return (
---
+++ src/Login.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { useForm } from '@mantine/form';
+import '@fontsource/inter';
+import {
+  Container,
+  Title,
+  Anchor,
+  TextInput,
+  PasswordInput,
+  Group,
+  Paper,
+  Checkbox,
+  Text,
+  Button,
+} from '@mantine/core';
+
+function Login() {
+  // eslint-disable-next-line no-unused-vars
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+      terms: true,
+    },
+
+    validate: {
+      email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+      password: (val) => (val.length <= 6
+        ? 'Password should include at least 6 characters'
+        : null),
+    },
+  });
+
+  return (
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Welcome back!
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Do not have an account yet?
+        {' '}
+        <Anchor size="sm" component="button">
+          Create account
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@upenn.edu" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group position="apart" mt="lg">
+          <Checkbox label="Remember me" />
+          <Anchor component="button" size="sm">
+            Forgot password?
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+      </Paper>
+    </Container>
+  );
+}
+
+export default Login;
