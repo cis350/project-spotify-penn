d42358f02bd328a4f432d3196cc31fd103e1aee1
Dzung Than <baodung2503@gmail.com>
2023-03-23 00:09:11 -0400

    app update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline} from '@mui/material'
 
-function App() {
+
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import UserProfile from "./pages/UserProfile";
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: 'black'}}>
+        <CssBaseline />
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<UserProfile />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
-
-export default App;
