35cfa2bc96b9e93a47b91c2257810829a38210a5
Maggie Schwierking <mschwier@wharton.upenn.edu>
2023-03-22 22:45:51 -0400

    testing for login

---
+++ src/tests/getUser.test.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import getUsers from '../api/getLoginUser';
+
+const mockAxios = new MockAdapter(axios);
+
+describe('the api returned correct data for admin@gmail.com', () => {
+  mockAxios.onGet().reply(200, {
+    id: 'admin@gmail.com', firstName: 'admin', lastName: 'admin', password: 'password',
+  });
+
+  test('the id is admin@gmail.com', async () => {
+    const data = await getUsers();
+    expect(data.id).toBe('admin@gmail.com');
+  });
+
+  test('the first name is correct', async () => {
+    const data = await getUsers();
+    expect(data.firstName).toBe('admin');
+  });
+
+  test('the last name is correct', async () => {
+    const data = await getUsers();
+    expect(data.lastName).toBe('admin');
+  });
+
+  test('the password is correct', async () => {
+    const data = await getUsers();
+    expect(data.firstName).toBe('password');
+  });
+});
