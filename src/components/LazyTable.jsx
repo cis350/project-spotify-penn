86eb7b4fef9da37a7faec53dddedeca75725ca2f
Maggie Schwierking <mschwier@wharton.upenn.edu>
2023-03-23 21:15:06 -0400

    testing leaderboard

---
+++ src/components/LazyTable.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+import {
+  Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow,
+} from '@mui/material';
+
+export default function LazyTable({
+  route, columns, defaultPageSize, rowsPerPageOptions,
+}) {
+  const [data, setData] = useState([]);
+
+  const [page, setPage] = useState(1); // 1 indexed
+  const [pageSize, setPageSize] = useState(defaultPageSize ?? 10);
+
+  useEffect(() => {
+    fetch(`${route}?page=${page}&page_size=${pageSize}`)
+      .then((res) => res.json())
+      .then((resJson) => setData(resJson));
+  }, [route, page, pageSize]);
+
+  const handleChangePage = (e, newPage) => {
+    if (newPage < page || data.length === pageSize) {
+      setPage(newPage + 1);
+    }
+  };
+
+  const handleChangePageSize = (e) => {
+    const newPageSize = e.target.value;
+
+    setPageSize(newPageSize);
+    setPage(1);
+  };
+
+  const defaultRenderCell = (col, row) => <div>{row[col.field]}</div>;
+
+  return (
+    <TableContainer style={{ backgroundColor: 'black' }}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((col) => <TableCell key={col.headerName} style={{ color: 'white' }}>{col.headerName}</TableCell>)}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, idx) => (
+            <TableRow key={idx}>
+              {
+                columns.map((col) => (
+                  <TableCell key={col.headerName} style={{ color: 'white' }}>
+                    {col.renderCell ? col.renderCell(row) : defaultRenderCell(col, row)}
+                  </TableCell>
+                ))
+              }
+            </TableRow>
+          ))}
+        </TableBody>
+        <TablePagination
+          rowsPerPageOptions={rowsPerPageOptions ?? [5, 10, 25]}
+          count={-1}
+          rowsPerPage={pageSize}
+          page={page - 1}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangePageSize}
+        />
+      </Table>
+    </TableContainer>
+  );
+}
