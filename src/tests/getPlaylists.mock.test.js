3cae43554791166daca99a9578bd166547d5d22d
Maggie Schwierking <mschwier@wharton.upenn.edu>
2023-03-23 21:44:10 -0400

    fixing leaderboard

--- src/tests/getPlaylists.mock.test.js
+++ src/tests/getPlaylists.mock.test.js
@@ -1,46 +1,45 @@
 import getPlaylists from '../api/getUserPlaylists';
-import getUsers from '../api/getUserPlaylists';
 
 jest.mock('../api/getUserPlaylists.jsx');
 
 getPlaylists.mockResolvedValue(
   [
     {
-      "id": "1",
-      "name": "Playlist 1",
-      "desc": "Hype songs"
+      id: '1',
+      name: 'Playlist 1',
+      desc: 'Hype songs',
     },
     {
-      "id": "2",
-      "name": "Playlist 2",
-      "desc": "Study jamss"
+      id: '2',
+      name: 'Playlist 2',
+      desc: 'Study jamss',
     },
     {
-      "id": "432nvsd",
-      "name": "hello",
-      "desc": "chill"
+      id: '432nvsd',
+      name: 'hello',
+      desc: 'chill',
     },
     {
-      "id": "abhijay",
-      "name": "abhijay",
-      "desc": "agarwal"
+      id: 'abhijay',
+      name: 'abhijay',
+      desc: 'agarwal',
     },
     {
-      "id": "vjfsak234",
-      "name": "benjams",
-      "desc": "jams for the ben"
+      id: 'vjfsak234',
+      name: 'benjams',
+      desc: 'jams for the ben',
     },
     {
-      "id": "43visf9vs0j3",
-      "name": "metal is heavy",
-      "desc": "heavy metal"
-    }
-  ]
+      id: '43visf9vs0j3',
+      name: 'metal is heavy',
+      desc: 'heavy metal',
+    },
+  ],
 );
 
 test('the first playlist is correct', async () => {
   const data = await getPlaylists();
-  expect(data[0].name).toBe("Playlist 1");
+  expect(data[0].name).toBe('Playlist 1');
 });
 
 test('the third playlist has correct id', async () => {
