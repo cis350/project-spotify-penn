6a39ff9dec857954b2fa3762b4200f65fc7ff21a
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2023-04-04 17:52:07 -0400

    chat feature

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,23 +2,63 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useRef, useEffect } from 'react';
 import {
-  Button, Textarea, Avatar, Badge, Title, Paper, Container, Center, Stack,
+  Button, Textarea, Avatar, Badge, Title, Paper, Container, Center, Stack, NumberInput,
 } from '@mantine/core';
+import {
+  getMessages, getSockets, newConversation, updateMessages,
+} from '../api/messages';
+import { getFirstName } from '../api/getUserData';
 
 function Chat() {
+  const currentUser = window.sessionStorage.getItem('sessionId');
+  const [userInitial, setUserInitial] = useState('');
+  const userName = getFirstName(currentUser).then((name) => setUserInitial(name[0].toUpperCase()));
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const messageContainerRef = useRef(null);
+  const [socket, setSocket] = useState(1);
+
+  // const initMessages = getMessages(1).then((arr) => {
+  //   setMessages(arr);
+  // });
 
   const handleInputChange = (event) => {
+    getMessages(socket).then((arr) => {
+      setMessages(arr);
+    });
     setInputValue(event.target.value);
   };
 
-  const handleSendMessage = () => {
-    const newMessage = { text: inputValue, sender: 'user' };
+  const handleSocketChange = (n) => {
+    getSockets().then((sockets) => {
+      if (sockets.includes(n)) {
+        setSocket(n);
+        getMessages(socket).then((arr) => {
+          setMessages(arr);
+        });
+      } else {
+        newConversation(n).then(() => {
+          setSocket(n);
+          getMessages(socket).then((arr) => {
+            setMessages(arr);
+          });
+        });
+      }
+    });
+  };
+
+  const handleSendMessage = async () => {
+    const newMessage = { text: inputValue, sender: currentUser };
     if (newMessage.text === '') return;
-    setMessages([...messages, newMessage]);
-    setInputValue('');
+
+    getMessages(socket).then((arr) => {
+      setMessages(arr);
+    });
+
+    updateMessages(socket, [...messages, newMessage]).then(() => {
+      setMessages([...messages, newMessage]);
+      setInputValue('');
+    });
   };
 
   const handleKeyDown = (event) => {
@@ -40,57 +80,71 @@ function Chat() {
       <Title
         align="center"
         size={60}
+        my={25}
         sx={(theme) => ({
-          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
           fontWeight: 900,
         })}
       >
         Chat with friends!
       </Title>
-      <Paper withBorder shadow="md" p={30} radius="md">
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignContent: 'center',
-            maxHeight: '300px',
-            minHeight: '300px',
-            overflow: 'auto',
-          }}
-          ref={messageContainerRef}
-        >
-          {messages.map((message, index) => (
+      <Center>
+        <Stack>
+          <NumberInput
+            defaultValue={1}
+            min={1}
+            max={10}
+            placeholder="Socket Number (1-10)"
+            label="Choose your socket"
+            value={socket}
+            onChange={handleSocketChange}
+            withAsterisk
+          />
+          <Paper w={600} withBorder shadow="md" p={30} radius="md">
             <div
-              key={index}
               style={{
                 display: 'flex',
-                justifyContent: message.sender === 'user' ? 'flex-end' : 'flex-start',
-                marginBottom: '8px',
+                flexDirection: 'column',
+                alignContent: 'center',
+                maxHeight: '300px',
+                minHeight: '300px',
+                overflow: 'auto',
               }}
+              ref={messageContainerRef}
             >
-              {message.sender !== 'user' && (
-              <Avatar style={{ marginRight: '8px' }}>R</Avatar>
-              )}
-              <Badge color={message.sender === 'user' ? 'gray' : 'teal'}>
-                {message.text}
-              </Badge>
-              {message.sender === 'user' && (
-              <Avatar style={{ marginLeft: '8px' }}>U</Avatar>
-              )}
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  style={{
+                    display: 'flex',
+                    justifyContent: message.sender === currentUser ? 'flex-end' : 'flex-start',
+                    marginBottom: '8px',
+                  }}
+                >
+                  {message.sender !== currentUser && (
+                  <Avatar style={{ marginRight: '8px' }}>U</Avatar>
+                  )}
+                  <Badge color={message.sender === currentUser ? 'gray' : 'teal'}>
+                    {message.text}
+                  </Badge>
+                  {message.sender === currentUser && (
+                  <Avatar style={{ marginLeft: '8px' }}>{userInitial}</Avatar>
+                  )}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <Textarea
-            value={inputValue}
-            onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
-            placeholder="Type your message here..."
-            style={{ flex: '1', marginRight: '8px' }}
-          />
-          <Button onClick={handleSendMessage}>Send</Button>
-        </div>
-      </Paper>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Textarea
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+                placeholder="Type your message here..."
+                style={{ flex: '1', marginRight: '8px' }}
+              />
+              <Button onClick={handleSendMessage}>Send</Button>
+            </div>
+          </Paper>
+        </Stack>
+      </Center>
 
     </>
   );
