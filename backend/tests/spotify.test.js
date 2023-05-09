3977d1a66afb1ba5af3bf91fdc99dce3e86ed129
abhijay-agarwal <57358174+abhijay-agarwal@users.noreply.github.com>
2023-05-09 00:14:01 -0400

    spotify test

---
+++ backend/tests/spotify.test.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const webapp = require('../server');
+
+describe('PUT /songs/:id', () => {
+  it('should respond with a success message when updating user songs', async () => {
+    const userId = 'admin@gmail.com'; // Replace with a valid user ID
+    const response = await request(webapp).put(`/songs/${userId}`).send({ songs: [] });
+    expect(response.statusCode).toBe(200);
+    // Add any other relevant properties you expect to be returned in the response body
+  });
+
+  it('should respond with an error message when trying to update songs for a non-existent user', async () => {
+    const userId = 'nonexistent@example.com';
+    const response = await request(webapp).put(`/songs/${userId}`).send({ songs: [] });
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toContain('User with ID');
+  });
+});
+
+describe('PUT /artists/:id', () => {
+  it('should respond with a success message when updating user artists', async () => {
+    const userId = 'admin@gmail.com'; // Replace with a valid user ID
+    const response = await request(webapp).put(`/artists/${userId}`).send({ artists: [] });
+    expect(response.statusCode).toBe(200);
+    // Add any other relevant properties you expect to be returned in the response body
+  });
+
+  it('should respond with an error message when trying to update artists for a non-existent user', async () => {
+    const userId = 'nonexistent@example.com';
+    const response = await request(webapp).put(`/artists/${userId}`).send({ artists: [] });
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toContain('User with ID');
+  });
+});
